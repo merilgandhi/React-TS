25b4850675790f11b8ad161c94310304bd8b9202
Meril Gandhi <meril.gandhi03@gmail.com>
2025-12-10 12:43:42 +0530

    Edited iconse and change routes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FiBox, FiLayers, FiEdit2, FiChevronDown, FiPackage } from "react-icons/fi";
+
+import {
+  FiShoppingBag,
+  FiList,
+  FiGitMerge,
+  FiChevronDown,
+  FiUserCheck,
+} from "react-icons/fi";
 
 const Sidebar: React.FC = () => {
   const [openProducts, setOpenProducts] = useState(false);
@@ -12,32 +19,39 @@ const Sidebar: React.FC = () => {
 
       <nav className="space-y-3">
 
+        {/* PRODUCTS DROPDOWN */}
         <div>
           <button
             className="flex items-center justify-between w-full text-left hover:text-amber-300"
             onClick={() => setOpenProducts(!openProducts)}
           >
             <span className="flex items-center space-x-2">
-              <FiBox />
+              <FiShoppingBag size={18} />
               <span>Manage Product</span>
             </span>
-            <FiChevronDown className={`${openProducts ? "rotate-180" : ""} transition`} />
+            <FiChevronDown
+              className={`${openProducts ? "rotate-180" : ""} transition`}
+            />
           </button>
 
           {openProducts && (
             <div className="ml-6 mt-2 space-y-2">
+
+              {/* PRODUCT LIST */}
               <Link
                 to="/products/edit"
                 className="flex items-center space-x-2 hover:text-amber-300"
               >
-                <FiEdit2 />
+                <FiList size={16} />
                 <span>Products List</span>
               </Link>
-            <Link
+
+              {/* VARIATIONS */}
+              <Link
                 to="/products/variations"
                 className="flex items-center space-x-2 hover:text-amber-300"
               >
-                <FiLayers />
+                <FiGitMerge size={16} />
                 <span>Variations</span>
               </Link>
 
@@ -45,12 +59,13 @@ const Sidebar: React.FC = () => {
           )}
         </div>
 
+        {/* SELLERS */}
         <Link
           to="/stock"
           className="flex items-center space-x-2 hover:text-amber-300"
         >
-          <FiPackage />
-          <span>Stock</span>
+          <FiUserCheck size={18} />
+          <span>Seller</span>
         </Link>
 
       </nav>
